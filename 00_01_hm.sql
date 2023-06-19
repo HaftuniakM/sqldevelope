10f0578281df49f9333bcb38403de88391175b5f
Maria Haftuniak <110645835+HaftuniakM@users.noreply.github.com>
2023-06-19 15:50:18 +0300

    Add files via upload

---
+++ 00_01_hm.sql
@@ -0,0 +1,29 @@
+SELECT *
+  FROM HR.jobs jb;
+
+SELECT * 
+  FROM HR.countries c;
+
+SELECT * 
+  FROM HR.departments d;
+
+SELECT *
+  FROM HR.employees emp;
+
+SELECT * 
+  FROM HR.job_history jh;
+
+SELECT * 
+  FROM HR.locations l;
+  
+SELECT * 
+  FROM HR.products p;
+
+SELECT *
+  FROM HR.products_old po;
+
+SELECT * 
+  FROM HR.regions r;
+
+SELECT * 
+  FROM HR.sales;
